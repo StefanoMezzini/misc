36f539c660f03b3ec6a0afc9c99c82cc0dc457d0
Stefano Mezzini <stefano@menouno.com>
2022-08-15 11:14:03 -0700

    added crop() and comments

--- cropping-raster-to-borders.R
+++ cropping-raster-to-borders.R
@@ -1,26 +1,37 @@
-library('tidyr')
-library('dplyr')
-library('raster')
-library('sf')
-library('spData')
-library('ggplot2')
+library('dplyr')          # for data wrangling
+library('raster')         # for raster data
+library('sf')             # for simple features
+library('rgeoboundaries') # for country/world boundaries
+library('ggplot2')        # for fancy plots
 
-K <- 100
-r <- expand_grid(x = seq(-180, 180, length.out = K),
-                 y = seq(30, 90, length.out = K)) %>%
-  mutate(z = sinpi(x / 15) * sinpi(y / 15)) %>%
-  rasterFromXYZ()
+# select a country's boundaries for cropping
+map_boundary <- geoboundaries(country = 'Italy')
+
+# create an arbitrary
+K <- 1500 # number of pixels in each direction (longitude, latitude)
+r <- tidyr::expand_grid(x = seq(-180, 180, length.out = K),
+                        y = seq(-90, 90, length.out = K)) %>%
+  mutate(z = sinpi(x / 15) * sinpi(y / 15)) %>% # add a column for fill
+  rasterFromXYZ() # convert from tibble to raster
 
 plot(r)
 
-r2 <- mask(r, world)
+# set values outside the boundaries to NA
+r2 <- mask(r, map_boundary)
 plot(r2)
 
+# remove values outside the boundaries
+r3 <- crop(r2, map_boundary)
+plot(r3)
+
+# convert back to tibble format
 r3 <- rasterToPoints(r2) %>%
   as_tibble()
 
+# plot the masked and cropped raster
 ggplot(r3, aes(x, y, fill = z)) +
   geom_tile() +
-  coord_map('azequidistant') +
+  coord_map('mercator') +
   cowplot::theme_map() +
-  theme(legend.position = 'none')
+  theme(legend.position = 'none') +
+  scale_fill_viridis_c()
