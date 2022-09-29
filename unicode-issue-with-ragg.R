708c1a8e5a68fc3f0af10702ea209aa95641549a
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2022-09-29 16:03:13 -0700

    code used to submit `ragg` issue

---
+++ unicode-issue-with-ragg.R
@@ -0,0 +1,25 @@
+library('ragg')
+plot(1, xlab = '\U1D53C doesn\'t work')
+ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(1, 1)) +
+  ggplot2::xlab('\U1D53C doesn\'t work')
+
+# restart R (Ctrl + Shift + F10)
+
+plot(1, xlab = '\U1D53C')
+library('ragg')
+plot(1, xlab = '\U1D53C now works')
+ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(1, 1)) +
+  ggplot2::xlab('\U1D53C now works')
+
+# restart R (Ctrl + Shift + F10)
+
+ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(1, 1)) +
+  ggplot2::xlab('\U1D53C')
+library('ragg')
+plot(1, xlab = '\U1D53C now works')
+ggplot2::ggplot() +
+  ggplot2::geom_point(ggplot2::aes(1, 1)) +
+  ggplot2::xlab('\U1D53C now works')
