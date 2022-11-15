a51ad860def0b66a1d5d229ff950593c6b1bcced
Stefano Mezzini <stefano@menouno.com>
2022-11-15 10:10:52 -0800

    Revert "added yearly trend in mean, changed variance trend"
    This reverts commit c652a18b8f1734658053833256c4e5541b59eaf5.

--- canadian-provinces/canada-sk-ab-map.R
+++ canadian-provinces/canada-sk-ab-map.R
@@ -9,48 +9,15 @@ theme_set(theme_void())
 # https://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/bound-limit-2016-eng.cfm
 
 # import canada shapefile
-canada.sf <- readOGR('misc/canadian-provinces/lpr_000b16a_e.shp', verbose = FALSE) %>%
+canada <- readOGR('free-time-shenanigans/canadian-provinces/lpr_000b16a_e.shp',
+                  verbose = FALSE) %>%
   st_as_sf()
 
 # subset provinces
-sk <- subset(canada.sf, PRENAME == 'Saskatchewan') %>%
-  as_Spatial() %>%
-  fortify() %>%
-  as_tibble()
-ab <- subset(canada.sf, PRENAME == 'Alberta') %>%
-  as_Spatial() %>%
-  fortify() %>%
-  as_tibble()
+sk <- subset(canada, PRENAME == 'Saskatchewan')
+ab <- subset(canada, PRENAME == 'Alberta')
 
-canada <- map_data('world', region = 'Canada') 
-
-# without data, geom_map() only plots SK and AB
 ggplot() +
-  coord_map(projection = 'albers', parameters =c(50, 70)) +
-  geom_polygon(aes(long, lat, group = group), canada, fill = 'grey', col = 'black',
-               size = 0.5) +
-  geom_polygon(aes(long, lat), sk, color = 'red', fill = 'transparent') +
-  geom_polygon(aes(long, lat), ab, color = 'red', fill = 'transparent')
-
-# geom_sf() plots all of canada, but this may take a while...
-ggplot() +
-  geom_sf(aes(geometry = geometry), canada.sf) +
+  geom_sf(aes(geometry = geometry), canada) +
   geom_sf(aes(geometry = geometry), sk, color = 'red') +
   geom_sf(aes(geometry = geometry), ab, color = 'red')
-
-# with data, geom_map() plots the relevant parts of canada
-range(canada$long)
-range(canada$lat)
-
-tibble(long = c(-140, -100, -50),
-       lat = c(42, 80, 45)) %>%
-  ggplot() +
-  geom_map(map = canada, aes(group = group, map_id = region), canada,
-           fill = 'grey', col = 'black', size = 0.5) +
-  geom_sf(aes(geometry = geometry), sk, color = 'red', stat = 'sf') +
-  geom_sf(aes(geometry = geometry), ab, color = 'red', stat = 'sf') +
-  geom_point(aes(long, lat), color = 'red')
-
-as_Spatial(sk) %>%
-  fortify() %>%
-  as_tibble()
